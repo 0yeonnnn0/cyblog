386b2d70d36fc4e3f10e654635a1486ec1b8c27c
lemonsoda1 <angrybird2600@gmail.com>
2024-10-20 15:54:27 -0700

    [BE] 회원가입 기능 구현

---
+++ src/app/api/auth/join/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../../lib/firebase";
+
+export async function POST(req: Request) {
+
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+
+    return NextResponse.json(
+      { message: "User created successfully", user },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
+
+export async function GET(req: Request) {
+    return NextResponse.json({ message: '잘 되는뎅?' });
+  }
+  
