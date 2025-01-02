21345c7a620a3a8f143549f8ab78bb5d50af4a83
lemonsoda1 <angrybird2600@gmail.com>
2025-01-02 13:40:14 -0800

    [feat] layout.tsx to same design with mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="bg-custom-gray bg-custom-bg h-[700px] overflow-x-auto font-dungGeunMo font-normal">
         <div className="flex ml-5 mt-5">
           <div className="basis-60 rounded-3xl bg-white border-gray-500 border-solid border-r-0 p-outerLeft border-2">
-            <div className="bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-left ml-4 border-r-0">
+            <div className="bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-left ml-3 border-r-0">
               <VisitorCounter />
 
               <div className="bg-white border-solid border-gray-400 border-3 rounded-2xl h-content w-content-left m-content-left p-content-left">
@@ -23,8 +23,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               </div>
             </div>
           </div>
-          <div className="basis-outerRight float-left rounded-3xl bg-white border-gray-500 border-solid border-l-0 pt-4 pb-4 border-2">
-            <div className="w-dotRight bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-right float-left border-l-0">
+          <div className="basis-outerRight rounded-3xl pr-3 bg-white border-gray-500 border-solid border-l-0 pt-4 pb-4 border-2">
+            <div className="w-dotRight bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-right border-l-0">
               <BlogTitle />
               <HPPostIt />
               <div className="w-content-right bg-white border-solid border-gray-400 border-3 h-content rounded-2xl m-content-right p-content-right">
