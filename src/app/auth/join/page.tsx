afeec191cc49307baa2e6a7d4d2a1b6bd609d7d1
lemonsoda1 <angrybird2600@gmail.com>
2025-01-02 03:11:47 -0800

    [fix] change join

--- src/app/auth/join/page.tsx
+++ src/app/auth/join/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { JoinForm } from "@/features/auth/components/join/JoinForm";
+// import { JoinForm } from "@/features/auth/components/join/JoinForm";
 
 export default function JoinPage() {
   // return <JoinForm />;
