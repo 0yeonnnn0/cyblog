3851cf8cad755170d7c1d658edfbebd017c0ddef
lemonsoda1 <angrybird2600@gmail.com>
2025-01-02 13:37:32 -0800

    [feat] layout.tsx for same design at mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className="bg-custom-gray bg-custom-bg h-[700px] overflow-x-auto font-dungGeunMo font-normal">
         <div className="flex ml-5 mt-5">
-          <div className="basis-60 float-right rounded-3xl bg-white border-gray-500 border-solid border-r-0 p-outerLeft border-2">
-            <div className="bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-left float-right border-r-0">
+          <div className="basis-60 rounded-3xl bg-white border-gray-500 border-solid border-r-0 p-outerLeft border-2">
+            <div className="bg-gray-300 bg-clip-content border-dashed border-gray-500 border-2 rounded-3xl p-dot-left ml-4 border-r-0">
               <VisitorCounter />
 
               <div className="bg-white border-solid border-gray-400 border-3 rounded-2xl h-content w-content-left m-content-left p-content-left">
